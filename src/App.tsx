ae6fd2faac25b00caecb66ad5774854e13dcf2f7
Cleverman0987 <cleverman0988@gmail.com>
2022-03-14 15:08:36 +0300

    Add my link :>

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,11 @@ import Project from "./components/Project";
 const App = () => (
   <React.Fragment>
     <Project />
+    <a
+      className="block mx-auto mt-6 mb-4 text-sm font-bold w-fit"
+      href="https://github.com/devmotheg">
+      Coded by Mohamed Muntasir
+    </a>
   </React.Fragment>
 );
 
